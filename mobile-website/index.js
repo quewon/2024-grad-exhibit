5eb102ef2b4e2a7d79cf6591834b66521d88ffaf
quewon <36914642+quewon@users.noreply.github.com>
2024-11-24 08:16:13 +0900

    .

--- mobile-website/index.js
+++ mobile-website/index.js
@@ -13,6 +13,13 @@ const app = express();
 
 //
 
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+});
 app.use(compression({ level: 1 }));
 app.use('/photos', express.static(path.join(__dirname, 'photos')));
 app.use('/', express.static(path.join(__dirname, 'public')));
